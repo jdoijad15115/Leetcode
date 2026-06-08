3c735fc99eb5a329fc4aa3aaf37c7b9274b348b8
Janhavi Doijad <janhavidoijad@gmail.com>
2026-06-08 14:46:40 +0530

    Time: 4 ms (82.26%), Space: 45.9 MB (49.04%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,34 +3,9 @@ class Solution {
         if (s.length()!=t.length()){
             return false;
         }
-        HashMap<Character, Integer> map = new HashMap<>();
-
-        for (int i = 0; i < s.length(); i++) {
-
-            char ch = s.charAt(i);
-
-            if (map.containsKey(ch)) {
-                map.put(ch, map.get(ch) + 1);
-            } else {
-                map.put(ch, 1);
-            }
-        }
-
-        for (int i = 0; i < t.length(); i++) {
-
-            char ch = t.charAt(i);
-
-            if (!map.containsKey(ch)) {
-                return false;
-            }
-
-            map.put(ch, map.get(ch) - 1);
-
-            if (map.get(ch) == 0) {
-                map.remove(ch);
-            }
-        }
-
-        return map.isEmpty();
-    }
-}
+        char [] a = s.toCharArray();
+        char [] b =t.toCharArray();
+        Arrays.sort(a);
+        Arrays.sort(b);
+        return Arrays.equals(a,b);
+}}
